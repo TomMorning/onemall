91f3efc81081ec74c9127b4d994a8331a50ed18b
TomMorning <376982595@qq.com>
2022-08-06 02:26:38 +0800

    Project initialization

---
+++ src/main/java/com/tangmaolin/onemall/OnemallApplication.java
@@ -0,0 +1,13 @@
+package com.tangmaolin.onemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnemallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tangmaolin/onemall/OnemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangmaolin.onemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnemallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
